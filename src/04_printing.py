980c88c29c11b2e5a8e3d5323f37051d15e42efa
Josh Cooter <thejoshcooter@gmail.com>
2020-02-12 01:38:54 -0600

    complete printing.py

--- src/04_printing.py
+++ src/04_printing.py
@@ -4,14 +4,14 @@ how to print using the printf operator, the `format` string
 method, and by using f-strings.
 """
 
-x = 10
-y = 2.24552
-z = "I like turtles!"
-
 # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
 
+print('x is % 2d, y is % 3f' % (10, 2.25))
+
 # Use the 'format' string method to print the same thing
+
+print('x is {}, y is {}, z is {}'.format(10, 2.25, "I like turtles!"))
 
 # Finally, print the same thing using an f-string
